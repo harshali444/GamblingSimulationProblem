9ce39ecb465dc79ffb16e40e97b74a9fef8b53b6
harshali444 <jadhav.h0564@gmail.com>
2020-10-29 09:49:13 +0530

    GamblingSimulation file added

--- GamblingSimulationProblem/src/com/Bridgelabz/GamblingSimulator/GamblingSimulator.java
+++ GamblingSimulationProblem/src/com/Bridgelabz/GamblingSimulator/GamblingSimulator.java
@@ -2,11 +2,13 @@ package com.Bridgelabz.GamblingSimulator;
 
 public class GamblingSimulator {
 	
+    final int STAKE = 100;
+    final int BETS = 1; 
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Gambling");
-	    final int STAKE = 100;
-	    final int BETS = 1;
-	}
+		
+    }
 
 }
