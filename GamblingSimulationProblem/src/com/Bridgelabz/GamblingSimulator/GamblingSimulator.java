e9414616297cfb6db98ff8ce2d922e257478673b
harshali444 <jadhav.h0564@gmail.com>
2020-10-29 10:35:03 +0530

    player will continue or not

--- GamblingSimulationProblem/src/com/Bridgelabz/GamblingSimulator/GamblingSimulator.java
+++ GamblingSimulationProblem/src/com/Bridgelabz/GamblingSimulator/GamblingSimulator.java
@@ -4,14 +4,16 @@ public class GamblingSimulator {
 	
     final int STAKE = 100;
     final int BETS = 1; 
+    int GOAL = 50;
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Gambling");
 		GamblingSimulator Gambling = new GamblingSimulator();
 		Gambling.winOrLoose();
-		
-	}	
+		Gambling.calculativeGambler();
+
+		}	
 		
 	public void winOrLoose() {
 		
@@ -20,7 +22,19 @@ public class GamblingSimulator {
 			System.out.println("Player is Winner");
 		else
 			System.out.println("Player Loose the game");
+	}
+	
+	public void calculativeGambler() {
 
+		
+		int RandomCheck = (int)Math.round(Math.random()*100);
+		
+		if(GOAL < RandomCheck)
+			System.out.println("player will continue game");
+		else
+			System.out.println("player will drop out");
+
+	    
 		
     }
 
