b1c07c0d89d60c6235041610b372aea83d273e45
harshali444 <jadhav.h0564@gmail.com>
2020-10-29 09:59:55 +0530

    Player is winner or looser

--- GamblingSimulationProblem/src/com/Bridgelabz/GamblingSimulator/GamblingSimulator.java
+++ GamblingSimulationProblem/src/com/Bridgelabz/GamblingSimulator/GamblingSimulator.java
@@ -8,6 +8,19 @@ public class GamblingSimulator {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Gambling");
+		GamblingSimulator Gambling = new GamblingSimulator();
+		Gambling.winOrLoose();
+		
+	}	
+		
+	public void winOrLoose() {
+		
+		int Random = (int)Math.round(Math.random()*10)%2;
+		if(Random == 1)
+			System.out.println("Player is Winner");
+		else
+			System.out.println("Player Loose the game");
+
 		
     }
 
